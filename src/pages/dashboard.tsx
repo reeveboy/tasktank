c6928e32ab65bc0d4b66e44d9ecdfc1cf48ed931
Ashbourn D'Cunha <94105472+cristan02@users.noreply.github.com>
2023-03-22 15:10:05 +0530

    revert

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,11 +1,14 @@
 import { NextPage } from "next";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+<<<<<<< HEAD
 import { useEffect } from "react";
 
 import Layout from "~/Components/Layout";
 
 import { api } from "~/utils/api";
+=======
+>>>>>>> parent of aabbab8 (more routers)
 
 const Dashboard: NextPage = () => {
   const router = useRouter();
@@ -17,6 +20,7 @@ const Dashboard: NextPage = () => {
     },
   });
 
+<<<<<<< HEAD
   // const { data } = api.team.getAll.useQuery();
   // const { mutate, error } = api.team.create.useMutation();
 
@@ -37,6 +41,11 @@ const Dashboard: NextPage = () => {
       <Layout session={session} route="Tracker">
         hello
       </Layout>
+=======
+  return (
+    <div>
+      <h1>Dashboard</h1>
+>>>>>>> parent of aabbab8 (more routers)
     </div>
   );
 };
