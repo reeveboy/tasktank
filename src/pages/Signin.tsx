c48af91dcff85a3f9656818319bfb79c02637f03
Ashbourn D'Cunha <94105472+cristan02@users.noreply.github.com>
2023-03-22 14:15:05 +0530

    signin/signup

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -13,7 +13,7 @@ function Signin() {
 
 
                 <p className='p-2'></p>
-                <div className='flex w-full relative p-0 grid '>
+                <div className='w-full relative p-0 grid '>
                     <input type="email" placeholder='Enter Email' className='px-3 py-2 w-full border rounded pr-9'></input>
                     <div className='absolute justify-self-end self-center mr-1 bg-white p-2'>
                     <svg className='w-3.5' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z"/></svg>
@@ -21,7 +21,7 @@ function Signin() {
                 </div>
                 <p className='p-2'></p>
 
-                <div className='flex w-full relative p-0 grid '>
+                <div className='w-full relative p-0 grid '>
                     <input type="password" placeholder='Password' className='px-3 py-2 w-full border rounded pr-9'></input>
                     <div className='absolute justify-self-end self-center mr-1 bg-white p-2'>
                     <svg className='w-4 ' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path d="M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7L525.6 386.7c39.6-40.6 66.4-86.1 79.9-118.4c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C465.5 68.8 400.8 32 320 32c-68.2 0-125 26.3-169.3 60.8L38.8 5.1zm151 118.3C226 97.7 269.5 80 320 80c65.2 0 118.8 29.6 159.9 67.7C518.4 183.5 545 226 558.6 256c-12.6 28-36.6 66.8-70.9 100.9l-53.8-42.2c9.1-17.6 14.2-37.5 14.2-58.7c0-70.7-57.3-128-128-128c-32.2 0-61.7 11.9-84.2 31.5l-46.1-36.1zM394.9 284.2l-81.5-63.9c4.2-8.5 6.6-18.2 6.6-28.3c0-5.5-.7-10.9-2-16c.7 0 1.3 0 2 0c44.2 0 80 35.8 80 80c0 9.9-1.8 19.4-5.1 28.2zm9.4 130.3C378.8 425.4 350.7 432 320 432c-65.2 0-118.8-29.6-159.9-67.7C121.6 328.5 95 286 81.4 256c8.3-18.4 21.5-41.5 39.4-64.8L83.1 161.5C60.3 191.2 44 220.8 34.5 243.7c-3.3 7.9-3.3 16.7 0 24.6c14.9 35.7 46.2 87.7 93 131.1C174.5 443.2 239.2 480 320 480c47.8 0 89.9-12.9 126.2-32.5l-41.9-33zM192 256c0 70.7 57.3 128 128 128c13.3 0 26.1-2 38.2-5.8L302 334c-23.5-5.4-43.1-21.2-53.7-42.3l-56.1-44.2c-.2 2.8-.3 5.6-.3 8.5z"/></svg>
